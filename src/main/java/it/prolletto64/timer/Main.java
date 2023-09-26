11020eddfb03f8dbed06f9edf3af2509a5cf4e2e
prolletto64 <poiluonzo@mail.com>
2023-09-26 19:07:21 +0200

    timer works
    Signed-off-by: prolletto64 <poiluonzo@mail.com>

--- src/main/java/it/prolletto64/timer/Main.java
+++ src/main/java/it/prolletto64/timer/Main.java
@@ -2,6 +2,8 @@ package it.prolletto64.timer;
 
 import javax.swing.*;
 import java.awt.*;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 class Main {
     public static void main(String[] args) {
@@ -14,7 +16,7 @@ class Main {
         frame.setVisible(true);
         frame.setIconImage(new ImageIcon("res/icon.png").getImage());
         JLabel label1 = new JLabel("ciao");
-        JLabel label2 = new JLabel("ciao ciao");
+        JLabel label2 = new JLabel("maybe will put something");
         frame.setLayout(new GridLayout(2, 1));
         label1.setFont(label1.getFont().deriveFont(22.0f));
         label2.setFont(label1.getFont());
@@ -30,5 +32,39 @@ class Main {
         label1.setForeground(text);
         label2.setForeground(text);
         frame.repaint();
+        LocalDateTime now;
+        LocalDateTime tmp = LocalDateTime.now(ZoneId.systemDefault());
+        int tmpSeconds2 = 61;
+        String etichetta1;
+        while (true) {
+            now = LocalDateTime.now(ZoneId.systemDefault());
+            if (now.getHour() < 10) {
+                etichetta1 = "0" + now.getHour() + ":";
+            } else {
+                etichetta1 = now.getHour() + ":";
+            }
+            if (now.getMinute() < 10) {
+                etichetta1 += "0" + now.getMinute() + ":";
+            } else {
+                etichetta1 += now.getMinute() + ":";
+            }
+            if (now.getSecond() < 10) {
+                etichetta1 += "0" + now.getSecond();
+            } else {
+                etichetta1 += Integer.toString(now.getSecond());
+            }
+            label1.setText("ORA: " + etichetta1);
+            if (now.getSecond() != tmpSeconds2) {
+                tmp = tmp.withSecond(0);
+                frame.repaint();
+                tmpSeconds2 = now.getSecond();
+                System.gc();
+            }
+            try {
+                Thread.sleep(1000);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
